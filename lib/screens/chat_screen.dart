3f33fe37d001a678bf548198c348e9e6b04da9ab
Azmain Adel <azmainadel47@gmail.com>
2020-03-09 16:33:37 +0600

    :wrench: Refactor chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -6,6 +6,9 @@ import 'package:flutter/widgets.dart';
 
 import '../constants.dart';
 
+final _firestore = Firestore.instance;
+final _auth = FirebaseAuth.instance;
+
 class ChatScreen extends StatefulWidget {
   static String id = 'chat_screen';
 
@@ -14,11 +17,9 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final _auth = FirebaseAuth.instance;
-  final _firestore = Firestore.instance;
+  final messageTextController = TextEditingController();
 
   FirebaseUser loggedInUser;
-
   String messageText;
 
   @override
@@ -50,40 +51,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            StreamBuilder<QuerySnapshot>(
-              stream: _firestore.collection('messages').snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  final messagesFromFirestore = snapshot.data.documents;
-                  List<MessageBubble> messageBubbleList = [];
-
-                  for (var message in messagesFromFirestore) {
-                    final messageText = message.data['text'];
-                    final messageSender = message.data['sender'];
-
-                    final messageBubble = MessageBubble(
-                      text: messageText,
-                      sender: messageSender,
-                    );
-                    messageBubbleList.add(messageBubble);
-                  }
-
-                  return Expanded(
-                    child: ListView(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 10.0, vertical: 20.0),
-                      children: messageBubbleList,
-                    ),
-                  );
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.lightBlueAccent,
-                    ),
-                  );
-                }
-              },
-            ),
+            MessageStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -91,6 +59,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       style: TextStyle(
                         color: Colors.black54,
                       ),
@@ -102,6 +71,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () {
+                      messageTextController.clear();
                       _firestore.collection('messages').add({
                         'text': messageText,
                         'sender': loggedInUser.email,
@@ -132,13 +102,43 @@ class _ChatScreenState extends State<ChatScreen> {
       print(e);
     }
   }
+}
 
-/*  void getMessages() async {
-    try {
-      await for (var snapshot
-          in _firestore.collection('messages').snapshots()) {}
-    } catch (e) {
-      print(e);
-    }
-  }*/
+class MessageStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('messages').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final messagesFromFirestore = snapshot.data.documents;
+          List<MessageBubble> messageBubbleList = [];
+
+          for (var message in messagesFromFirestore) {
+            final messageText = message.data['text'];
+            final messageSender = message.data['sender'];
+
+            final messageBubble = MessageBubble(
+              text: messageText,
+              sender: messageSender,
+            );
+            messageBubbleList.add(messageBubble);
+          }
+
+          return Expanded(
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              children: messageBubbleList,
+            ),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+      },
+    );
+  }
 }
