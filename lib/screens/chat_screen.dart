de934faa1b51d9e7f79925927d95e4fb95a16d14
Azmain Adel <azmainadel47@gmail.com>
2020-03-09 16:28:19 +0600

    :wrench: Refactor components

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:Chatter/components/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -140,45 +141,4 @@ class _ChatScreenState extends State<ChatScreen> {
       print(e);
     }
   }*/
-}
-
-class MessageBubble extends StatelessWidget {
-  final String text;
-  final String sender;
-
-  MessageBubble({this.text, this.sender});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          Material(
-            borderRadius: BorderRadius.circular(30.0),
-            elevation: 6.0,
-            color: Colors.lightBlueAccent,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-              child: Text(
-                '$text',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 15.0,
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 4.0,
-          ),
-          Text(
-            'From $sender',
-            style: TextStyle(fontSize: 12.0, color: Colors.black54),
-          ),
-        ],
-      ),
-    );
-  }
 }
