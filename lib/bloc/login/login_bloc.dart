b02dde491318dd88836a0b46a573febb569a3792
Azmain Adel <azmainadel47@gmail.com>
2020-03-12 10:52:29 +0600

    :zap: Add login bloc event-state mapping

--- lib/bloc/login/login_bloc.dart
+++ lib/bloc/login/login_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:chatter/bloc/login/login_event.dart';
 import 'package:chatter/bloc/login/login_state.dart';
 import 'package:chatter/data/repositories/user_repository.dart';
+import 'package:chatter/ui/util/validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,8 +15,44 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   }
 
   @override
-  LoginState get initialState {}
+  LoginState get initialState => LoginState.empty();
 
   @override
-  Stream<LoginState> mapEventToState(LoginEvent event) {}
+  Stream<LoginState> mapEventToState(LoginEvent event) async* {
+    if (event is EmailChanged) {
+      yield* _mapEmailChangedToState(event.email);
+    } else if (event is PasswordChanged) {
+      yield* _mapPasswordChangedToState(event.password);
+    } else if (event is LoginWithCredentialsPressed) {
+      yield* _mapLoginWithCredentialsPressedToState(
+        email: event.email,
+        password: event.password,
+      );
+    }
+  }
+
+  Stream<LoginState> _mapEmailChangedToState(String email) async* {
+    yield state.update(
+      isEmailValid: Validator.isValidEmail(email),
+    );
+  }
+
+  Stream<LoginState> _mapPasswordChangedToState(String password) async* {
+    yield state.update(
+      isPasswordValid: Validator.isValidPassword(password),
+    );
+  }
+
+  Stream<LoginState> _mapLoginWithCredentialsPressedToState({
+    String email,
+    String password,
+  }) async* {
+    yield LoginState.loading();
+    try {
+      await _userRepository.signInWithCredentials(email, password);
+      yield LoginState.success();
+    } catch (_) {
+      yield LoginState.failure();
+    }
+  }
 }
