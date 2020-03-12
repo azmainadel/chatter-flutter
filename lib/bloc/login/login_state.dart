5409e4dddc40d8a2a683be82a55e7fc66f230f6e
Azmain Adel <azmainadel47@gmail.com>
2020-03-12 10:44:21 +0600

    :zap: Add login states

--- lib/bloc/login/login_state.dart
+++ lib/bloc/login/login_state.dart
@@ -16,4 +16,79 @@ class LoginState {
     @required this.isSuccess,
     @required this.isFailure,
   });
+
+  /*
+    From Dart Language tour:
+    Use the factory keyword when implementing a constructor
+    that does not always create a new instance of its class.
+  */
+
+  factory LoginState.empty() {
+    return LoginState(
+        isEmailValid: true,
+        isPasswordValid: true,
+        isSubmitting: false,
+        isSuccess: false,
+        isFailure: false);
+  }
+
+  factory LoginState.loading() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isSubmitting: true,
+      isSuccess: false,
+      isFailure: false,
+    );
+  }
+
+  factory LoginState.failure() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isSubmitting: false,
+      isSuccess: false,
+      isFailure: true,
+    );
+  }
+
+  factory LoginState.success() {
+    return LoginState(
+      isEmailValid: true,
+      isPasswordValid: true,
+      isSubmitting: false,
+      isSuccess: true,
+      isFailure: false,
+    );
+  }
+
+  LoginState update({
+    bool isEmailValid,
+    bool isPasswordValid,
+  }) {
+    return copyWith(
+      isEmailValid: isEmailValid,
+      isPasswordValid: isPasswordValid,
+      isSubmitting: false,
+      isSuccess: false,
+      isFailure: false,
+    );
+  }
+
+  LoginState copyWith({
+    bool isEmailValid,
+    bool isPasswordValid,
+    bool isSubmitEnabled,
+    bool isSubmitting,
+    bool isSuccess,
+    bool isFailure,
+  }) {
+    return LoginState(
+      isEmailValid: isEmailValid ?? this.isEmailValid,
+      isPasswordValid: isPasswordValid ?? this.isPasswordValid,
+      isSubmitting: isSubmitting ?? this.isSubmitting,
+      isSuccess: isSuccess ?? this.isSuccess,
+      isFailure: isFailure ?? this.isFailure,
+    );
+  }
 }
